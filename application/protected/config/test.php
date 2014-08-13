5e0fbfdf2547d181c17a9cceb7e5304fc98f7693
Phillip Shipley <phillip_shipley@sil.org>
2014-08-13 11:48:21 -0400

    starting to setup codeship.io

--- application/protected/config/test.php
+++ application/protected/config/test.php
@@ -16,5 +16,10 @@ return CMap::mergeArray(
             ),
             */
         ),
+        'params' => array(
+            "apiBaseUrl" => "http://demo7011044.mockable.io/search",
+            "apiKey" => "testkey",
+            "apiSecret" => "testsecret",
+        ),
     )
 );
